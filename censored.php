5e716cab5fc6d00459361ee4f6c0a25ed96c45ec
mariasolegallina <gallinamariasole@gmail.com>
2024-03-18 09:19:56 +0100

    add - style

--- censored.php
+++ censored.php
@@ -2,34 +2,43 @@
 $message = $_POST["message"];
 $bad_word = $_POST["bad-word"];
 
-$censored_message = str_replace( $bad_word, "xxx", $message );
+$censored_message = str_replace($bad_word, "***", $message);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Censurato</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/app.css">
 </head>
+
 <body>
-    <!-- stampare a schermo il messaggio -->
-    <h1>Ecco il tuo messaggio</h1>
-    <p><?php echo $message ?></p>
+    <div class="container">
+        <div class="my-3">
+            <!-- stampare a schermo il messaggio -->
+            <h2>Ecco il tuo messaggio</h2>
+            <p><?php echo $message ?></p>
 
-    <!-- stampare a schermo la lunghezza del messaggio -->
-    <p>
-        Hai scritto un messaggio di <?php echo strlen($message) ?> caratteri.
-    </p>
+            <!-- stampare a schermo la lunghezza del messaggio -->
+            <p>
+                Hai scritto un messaggio di <?php echo strlen($message) ?> caratteri.
+            </p>
 
-    <!-- stampare a schermo il testo con censura  -->
-    <h1>La censura</h1>
-    <p><?php echo $censored_message ?></p>
+            <!-- stampare a schermo il testo con censura  -->
+            <h2>La censura</h2>
+            <p><?php echo $censored_message ?></p>
 
-    <!-- stampare a schermo la lughezza del messaggio censurato -->
-    <p>
-        Il messaggio censurato ha <?php echo strlen($censored_message) ?> caratteri.
-    </p>
-    
+            <!-- stampare a schermo la lughezza del messaggio censurato -->
+            <p>
+                Il messaggio censurato ha <?php echo strlen($censored_message) ?> caratteri.
+            </p>
+        </div>
+    </div>
+
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,20 +1,37 @@
 <!DOCTYPE html>
 <html lang="it">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Censura</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/app.css">
 </head>
+
 <body>
-    <h1>Testo da censura</h1>
+    <div class="container">
+        <div class="my-3">
+            <h1>Testo da censura</h1>
 
-    <!-- creare un form PHP che permetta di inviare un messaggio e una parola da censurare -->
-    <form action="censored.php" method="POST">
-        <input type="text" name="message" placeholder="scrivi un messaggio">
-        <input type="text" name="bad-word" placeholder="scrivi la parola da censurare">
-        <input type="submit" value="Invia">
-    </form>
+            <!-- creare un form PHP che permetta di inviare un messaggio e una parola da censurare -->
+            <form action="censored.php" method="POST">
+                <div class="mb-3">
+                    <label class="form-label">Scrivi il tuo messaggio</label>
+                    <!-- <input class="form-control" type="text" name="message"> -->
+                    <textarea class="form-control" type="text" name="message"></textarea>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Quale parola vuoi censurare?</label>
+                    <input class="form-control" type="text" name="bad-word">
+                </div>
+                <button class="btn btn-primary" type="submit">Invia</button>
+            </form>
+        </div>
 
-    
+    </div>
+
+
 </body>
-</html> 
+
+</html>
